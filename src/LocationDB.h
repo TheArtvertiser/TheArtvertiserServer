36cb75c9b0d731b4b505ca5aae3e392320ff8666
arturo <arturo@openframeworks.cc>
2011-07-27 19:01:58 +0200

    license headers

--- src/LocationDB.h
+++ src/LocationDB.h
@@ -1,8 +1,22 @@
 /*
- * LocationDB.h
- *
- *  Created on: 30/04/2011
- *      Author: arturo
+ Copyright 2011 Arturo Castro <arturo@arturocastro.net>,
+
+ Distributed under the terms of the GNU General Public License v3.
+
+ This file is part of The Artvertiser Server.
+
+ The Artvertiser Server is free software: you can redistribute
+ it and/or modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ The Artvertiser Server is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with The Artvertiser for Android.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LOCATIONDB_H_
